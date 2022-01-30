cf6d837cead827295f3fca14164f5b252a742318
Wiktor Musial <wiktormusial@icloud.com>
2022-01-30 14:55:48 +0100

    Dispatch albums in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { getAlbumsStatus } from '../../slices/albums/albumsSlice'
+import { fetchAlbums } from '../../slices/albums/fetchAlbums'
+
 export default function Header() {
+  const dispatch = useDispatch()
+  const albumsStatus = useSelector(getAlbumsStatus)
+
+  useEffect(() => {
+    if (albumsStatus == 'idle') {
+      dispatch(fetchAlbums())
+    }
+  })
+
   return(
     <h1>Audier</h1>
   )
